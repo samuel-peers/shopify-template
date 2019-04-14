27c92ab82d29aaa240ba97d3bfe745b914235f60
samuel-peers <samuelpeers@gmail.com>
2019-04-14 18:11:56 -0500

    wrong syntax

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/* 
-&& aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
+zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/* &&
+aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
