1354161e57eb9843893ae9e4f4187ff7f21ee7ee
samuel-peers <samuelpeers@gmail.com>
2019-04-14 18:06:34 -0500

    dont use source

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-source zipper.sh && aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
+zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/* 
+&& aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
