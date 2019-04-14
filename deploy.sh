34fc4d358c44a197f58198a0464575ee47f8ce96
samuel-peers <samuelpeers@gmail.com>
2019-04-14 16:11:11 -0500

    actual build step

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/* \
-&& aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
+source zipper.sh && aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
---
+++ zipper.sh
@@ -0,0 +1,1 @@
+zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/*
