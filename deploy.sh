9fb5770474641444d7e18948f0e86438b7ef6af0
samuel-peers <samuelpeers@gmail.com>
2019-04-17 19:18:43 -0500

    moves build to deploysh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+./.env &&
+npm run build &&
 zip -r output.zip backend/dist/lambda-build.js frontend/dist/* -x node_modules/* &&
 aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://output.zip
