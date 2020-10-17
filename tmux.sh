c4046dc1765676c564b6d55abaefacdcaf67daf2
samuel-peers <samuelpeers@gmail.com>
2020-10-17 11:03:20 -0500

    updates tmux script to use new npm scripts

--- tmux.sh
+++ tmux.sh
@@ -1,13 +1,9 @@
 tmux new -d -s dev;
-tmux new -d -s watch-backend;
 tmux new -d -s run-backend;
 tmux new -d -s run-frontend;
 tmux send-keys -t dev "cd ${PWD}" Enter;
-tmux send-keys -t watch-backend "cd ${PWD}" Enter;
 tmux send-keys -t run-backend "cd ${PWD}" Enter;
 tmux send-keys -t run-frontend "cd ${PWD}" Enter;
-tmux send-keys -t watch-backend "npm run watch-backend" Enter;
-tmux send-keys -t run-backend "source .env" Enter;
-tmux send-keys -t run-backend "npm run run-backend" Enter;
-tmux send-keys -t run-frontend "npm run run-frontend" Enter;
-tmux a -t run-backend;
+tmux send-keys -t run-backend "cd backend && npm start" Enter;
+tmux send-keys -t run-frontend "cd frontend && npm start" Enter;
+tmux a -t dev;
